9c62305a0d3bff9a82a3f843290b2dd192c228e6
Caio <caio23guimaraes@gmail.com>
2019-07-11 17:30:13 -0300

    fix deploy images

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyB0uHvabUJYcOQXw8ad9URu3guRzdmbfG4",
     authDomain: "appteleidoso.firebaseapp.com",
